2e67fa7e094337b6b6098032dbef179dadb48956
Manuel Stadelmann <manuel-stadelmann@live.de>
2023-09-25 19:08:32 +0200

    Fehlerhafter test

--- MyAddApp.Tests/UnitTest1.cs
+++ MyAddApp.Tests/UnitTest1.cs
@@ -17,7 +17,22 @@ namespace MyAddApp.Tests
 			// act
 			var result = Calculator.Add(x, y);
 
+			// assert
 			Assert.That(result, Is.EqualTo(3));
+		}
+
+		[Test]
+		public void Test_Error()
+		{
+			// arrange
+			var x = 1;
+			var y = 2;
+
+			// act
+			var result = Calculator.Add(x, y);
+
+			// assert
+			Assert.That(result, Is.EqualTo(4));
 		}
 	}
 }
